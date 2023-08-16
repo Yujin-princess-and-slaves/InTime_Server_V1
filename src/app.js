38ffcd1dab5ef891e63e54b6a2bf7017b7802292
Yujin <jin37091289@gmail.com>
2023-08-17 00:35:00 +0900

    FEAT :: DB 연동

--- src/app.js
+++ src/app.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const app = express();
+const db = require("./models");
 const port = 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+db.sequelize
+    .sync()
+    .then(() => {
+        console.log("db connect");
+    })
+    .catch(console.error);
 
 app.listen(port, () => {
     console.log(`listening at http://localhost:${port}`);
