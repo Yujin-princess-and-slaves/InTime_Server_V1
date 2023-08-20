cbb18419670a776dbdd9d20e5e5844227fd48f8a
Yujin <jin37091289@gmail.com>
2023-08-21 00:46:40 +0900

    FEAT :: delete 라우터 추가

--- src/routes/boardRouter.js
+++ src/routes/boardRouter.js
@@ -15,7 +15,9 @@ router.put("/:boardId", async (req, res) => {
         });
 
         if (!board) {
-            return res.status(404).json({ error: "게시판을 찾을 수 없습니다" });
+            return res
+                .status(404)
+                .json({ success: false, error: "게시판을 찾을 수 없습니다" });
         }
 
         board.title = title;
@@ -24,7 +26,32 @@ router.put("/:boardId", async (req, res) => {
 
         await board.save();
 
-        res.status(200).json({ success: true, message: "게시판 업데이트 성공" });
+        res.status(200).json({
+            success: true,
+            message: "게시판 업데이트 성공",
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ success: false, error: "서버 에러" });
+    }
+});
+
+router.delete("/:boardId", async (req, res) => {
+    const boardId = req.params.boardId;
+
+    try {
+        const board = await models.board.findOne({
+            where: { boardId: boardId },
+        });
+        if (!board) {
+            return res
+                .status(404)
+                .json({ success: false, error: "게시판을 찾을 수 없습니다" });
+        }
+
+        await board.destroy();
+
+        res.status(200).json({ success: true });
     } catch (err) {
         console.log(err);
         res.status(500).json({ success: false, error: "서버 에러" });
