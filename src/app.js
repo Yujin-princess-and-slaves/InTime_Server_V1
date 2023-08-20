3f9e44ee9c07c0c2175bb46a791eb92f5b900eda
Yujin <jin37091289@gmail.com>
2023-08-21 00:36:36 +0900

    FEAT :: boardrouter와 연동

--- src/app.js
+++ src/app.js
@@ -2,9 +2,12 @@ const express = require("express");
 const app = express();
 const db = require("./models");
 const port = 5000;
+const boardRouter = require("./routes/boardRouter");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/board", boardRouter);
 
 db.sequelize
     .sync()
