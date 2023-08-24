ea59cf6b689d87f857c131f07466cecab7ca5ecc
hongseop <a01082372487@gmail.com>
2023-08-24 23:56:19 +0900

    test

--- src/routes/boardRouter.js
+++ src/routes/boardRouter.js
@@ -6,56 +6,56 @@ const models = require("../models");
 router.use(express.json());
 
 router.put("/:boardId", async (req, res) => {
-    const id = req.params.boardId;
-    const { title, content } = req.body;
+  const id = req.params.boardId;
+  const { title, content } = req.body;
 
-    try {
-        const board = await models.board.findOne({
-            where: { boardId: id },
-        });
+  try {
+    const board = await models.board.findOne({
+      where: { boardId: id },
+    });
 
-        if (!board) {
-            return res
-                .status(404)
-                .json({ success: false, error: "게시판을 찾을 수 없습니다" });
-        }
-
-        board.title = title;
-        board.content = content;
-        board.dataTypes = Sequelize.literal(`now()`);
-
-        await board.save();
-
-        res.status(200).json({
-            success: true,
-            message: "게시판 업데이트 성공",
-        });
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({ success: false, error: "서버 에러" });
+    if (!board) {
+      return res
+        .status(404)
+        .json({ success: false, error: "게시판을 찾을 수 없습니다" });
     }
+
+    board.title = title;
+    board.content = content;
+    board.dataTypes = Sequelize.literal(`now()`);
+
+    await board.save();
+
+    res.status(200).json({
+      success: true,
+      message: "게시판 업데이트 성공",
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ success: false, error: "서버 에러" });
+  }
 });
 
 router.delete("/:boardId", async (req, res) => {
-    const boardId = req.params.boardId;
+  const boardId = req.params.boardId;
 
-    try {
-        const board = await models.board.findOne({
-            where: { boardId: boardId },
-        });
-        if (!board) {
-            return res
-                .status(404)
-                .json({ success: false, error: "게시판을 찾을 수 없습니다" });
-        }
-
-        await board.destroy();
-
-        res.status(200).json({ success: true });
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({ success: false, error: "서버 에러" });
+  try {
+    const board = await models.board.findOne({
+      where: { boardId: boardId },
+    });
+    if (!board) {
+      return res
+        .status(404)
+        .json({ success: false, error: "게시판을 찾을 수 없습니다" });
     }
+
+    await board.destroy();
+
+    res.status(200).json({ success: true });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ success: false, error: "서버 에러" });
+  }
 });
 
 module.exports = router;
